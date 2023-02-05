4d290a9c637f83ecf492894dda2ed6c3e3931c23
LanceTreyark <github@treyark.com>
2023-02-05 06:50:01 -0800

    02.05.23 06:49AM

--- microInit_i1.sh
+++ microInit_i1.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
+#v.020523
 read -p "What is the remote origin for this new repository?   " gitOrigin1
-
-thisPath=$(pwd)
-
-
 git init
 git add .
-git commit -m "first commit"
+git commit -m "First Commit"
 git branch -M main
 git remote add origin $gitOrigin1
 git push -u origin main
