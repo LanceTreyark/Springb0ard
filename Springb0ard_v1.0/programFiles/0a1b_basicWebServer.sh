d3fd7877a234659060fb32e82b1badd2c76027ac
LanceTreyark <github@treyark.com>
2023-03-14 10:16:10 -0700

    03.14.23 10:16AM

---
+++ Springb0ard_v1.0/programFiles/0a1b_basicWebServer.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+#!!!!!!!!!!!!!!!!!!!!   KEEP IN MIND THIS IS A PUBLIC REPO  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+# nano 0a1b_basicWebServer.sh
+# sudo chmod +x 0a1b_basicWebServer.sh
+# ./0a1b_basicWebServer.sh
+
+<<comment
+* TITLE:  0a1b_basicWebServer.sh
+* AUTHOR: Lance Pierson
+* EMAIL:  info@treyark.com
+* DATE:   2/14/23
+* EXECUTIVE SUMMARY: 
+* UNIT TEST RESULT: 
+comment
+
+echo "The script is live..."
+sleep 1
+echo ""
+echo "The following script installs an Apache webserver, a UFW Firewall, opens the necessary WWW ports"
+echo "& installs Certbot a SSL Certificate generator."
+echo ""
+read -p "Press enter to proceed with the installation." xcfg
+sleep 2
+echo ""
+echo "Installing Uncomplicated Firewall, this may already be installed..."
+sudo apt install ufw -y
+sleep 1
+echo " "
+echo "Allow WWW ports 443 and 80"
+sudo ufw allow in "WWW Full"
+sleep 1
+echo " "
+echo "Check UFW Status:"
+sudo ufw status
+sleep 1
+echo " "
+echo "Installing Apache Webserver..."
+sudo apt install apache2 -y
+sleep 1
+echo " "
+echo "Installing dependencies for certbot"
+sleep 1
+echo ""
+echo "+ Snap D"
+sudo apt install snapd
+sleep 1
+echo " "
+echo "+ Snap Core"
+sudo snap install core
+sleep 1
+echo " "
+echo "Refresh Snap Core"
+sudo snap refresh core
+sleep 1
+echo " "
+echo "Installing Certbot"
+sudo snap install --classic certbot
+sleep 1
+echo " "
+echo "Adding symbolic link for Certbot"
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
+sleep 1
+echo "Installing apache pligin..."
+sudo service apache2 reload
+sudo apt install python3-certbot-apache -y
+echo "The script has concluded. Bye"
---
+++ Springb0ard_v1.0/programFiles/0a1c_configBasicWebServer.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+#!!!!!!!!!!!!!!!!!!!!   KEEP IN MIND THIS IS A PUBLIC REPO  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+# nano 0a1c_configBasicWebServer.sh
+# sudo chmod +x 0a1c_configBasicWebServer.sh
+# ./0a1c_configBasicWebServer.sh
+
+<<comment
+* TITLE:  0a1c_configBasicWebServer.sh
+* AUTHOR: Lance Pierson
+* EMAIL:  info@treyark.com
+* DATE:   3/14/23
+* EXECUTIVE SUMMARY: 
+* UNIT TEST RESULT: 
+comment
+
+echo "The script is live..."
+sleep 1
+echo ""
+echo "The following script configures an Apache webserver "
+echo "for a given domain name you provide, in addition it provides an SSL."
+echo "certificate."
+sleep 2
+echo ""
+read -p "Please enter the domain name to use for this new site, Don't add the 'www':   " webDomainName
+echo " "
+read -p "Enter your Administrative email to use for SSL Certification:   " webAdminEmail
+echo " "
+echo " "
+echo "If you have an existing subdomain for this same domain like 'mail.example.com', you can" 
+echo "enter that here to group everything under the same certificate"
+echo ""
+read -p "Do you have an existing subdomain with SSL certificate? [y/n] " has_ssl
+
+if [[ $has_ssl =~ ^[Yy]$ ]]; then
+  # If user has SSL, ask for the subdomain name
+  read -p "Enter existing subdomain name: " existing_subdomain
+
+  # Set variables accordingly
+  d="-d"
+  existingSubDomain="$existing_subdomain"
+else
+  # If user doesn't have SSL, set variables to empty
+  d=""
+  existingSubDomain=""
+fi
+#
+# Use variables in your script as needed
+#echo "d=$d"
+#echo "existingSubDomain=$existingSubDomain"
+#
+
+echo "Creating a site directory for $webDomainName"
+sudo mkdir -p /var/www/$webDomainName/public_html
+sleep 1
+echo " "
+echo "Create Apache2 configuration file"
+#cat >/etc/apache2/sites-available/$webDomainName.conf <<EOF
+cat >/tmp/$webDomainName.conf <<EOF
+<VirtualHost *:80>
+    ServerAdmin $webAdminEmail
+    ServerName $webDomainName
+    ServerAlias www.$webDomainName
+    DocumentRoot /var/www/$webDomainName/public_html
+    ErrorLog ${APACHE_LOG_DIR}/error.log
+    CustomLog ${APACHE_LOG_DIR}/access.log combined
+  <Directory /var/www/$webDomainName/public_html/>
+          AllowOverride All
+  </Directory>
+</VirtualHost>
+EOF
+sudo mv /tmp/$webDomainName.conf /etc/apache2/sites-available/
+sleep 1
+echo " "
+echo "Configure permissions for the Web directory"
+sudo chown -R www-data:www-data /var/www/$webDomainName/public_html
+sleep 1
+echo " "
+echo "Enable Website and Obtain SSL Certificate"
+sudo a2ensite $webDomainName.conf
+sleep 1
+echo " "
+echo "Restart Apache"
+sudo systemctl restart apache2
+sleep 1
+echo " "
+echo "Obtain SSL Certificate"
+sudo certbot --apache $d $existingSubDomain -d $webDomainName -d www.$webDomainName
+sleep 1
+echo " "
+echo "Restarting Apache..."
+sudo systemctl restart apache2
+echo "The script has concluded."
+echo "bye"
