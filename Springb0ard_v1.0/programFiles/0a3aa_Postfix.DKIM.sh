f232a7c833bcaae1b4f9cd5a711f509c89f7432b
LanceTreyark <github@treyark.com>
2023-03-18 10:08:10 -0700

    03.18.23 10:08AM

--- Springb0ard_v1.0/programFiles/0a3aa_Postfix.DKIM.sh
+++ Springb0ard_v1.0/programFiles/0a3aa_Postfix.DKIM.sh
@@ -27,127 +27,85 @@ sleep 1
 #sudo sed -i "/#Domain/a Domain                 $mailDomain" /etc/opendkim.conf
 #sudo sed -i "/#Selector/a Selector               2020" /etc/opendkim.conf
 #sudo sed -i "/#KeyFile/a KeyFile                /etc/dkimkeys/example.private" /etc/opendkim.conf
+
+# Make sure you are in the proper directory
 cd /home/$sudoUser
 
-# \/ This
+# \/ This Creates our DKIM Keys
 #sudo /usr/sbin/opendkim-genkey -b 2048 -d $mailDomain -s default
 
 
-# \/ This
+# \/ This creates a variable to hold the current users id # ie 1001
 sudoUserID=$(id -u $sudoUser)
 
-# \/ This
-sudo cp default.txt defaultXyz.txt
-
-### \/ This makes sure ownership is readable w/o sudo
-sudo chown -R $sudoUserID:$sudoUserID /home/$sudoUser/defaultXyz.txt
 
 #----------------------------------------------------------
-# THIS CODE IS NOT GOOD YET
+# Begin with DNS Syntaxing
 #----------------------------------------------------------
+#Here is the plan:
 
-# # \/ This Reads the contents of defaultx.txt into a variable
-contents=$(cat defaultx.txt)
+# PHASE 1) we copy the actual key file to a working file & make it writable
 
-# # \/ This Replacse line breaks with a space
-contents=${contents//$'\n'/' '}
+# \/ This copies the keys to a working file that we can modify or 
+#    delete without messing with the system
+sudo cp default.txt defaultX1.txt
+
+### \/ This makes sure our working file is writable for the corrent
+#       user because the original file is owned by root.
+sudo chown -R $sudoUserID:$sudoUserID /home/$sudoUser/defaultX1.txt
+
+# PHASE 2) we pluck out the key from the header and footer
+#!/bin/bash
+
+#sed -i '1s/^default._domainkey\s\+IN\s\+TXT\s\+(\s*"v=DKIM1; h=sha256; k=rsa; "\s*"p=[^"]*"\s*)\s*;\s*----- DKIM key default for caspardata.com\s*//' defaultX1.txt
+sed -i '/^default\._domainkey.*$/ {N; s/^[^\n]*\n[^\n]*"//}' defaultX1.txt
+
+
+
+
+# PHASE 3) remove quotes, spaces and new lines
+
+# \/\/ This command takes the contents of the file defaultX1.txt, 
+#      removes all newline characters from it, and then writes 
+#      the resulting output to a new file named defaultX2.txt.
+#cat defaultX1.txt | tr -d '\n' > defaultX2.txt
+
+# # # This command takes the data defaultX2.txt & replaces all
+# occurrences of whitespace (spaces and tabs) with nothing.
+# then saves the output in a new file named "defaultX3.txt".
+#sed 's/\s//g' defaultX2.txt > defaultX3.txt
+
+# PHASE 4) break up into 64 char lines and wrap in quotes
+
+# read input file into variable
+#input=$(cat defaultX3.txt)
+
+# remove newlines and replace with space
+#input=${input//$'\n'/ }
 
 # # \/ This Removes extra spaces and tabs
-contents=$(echo $contents | tr -s '[:blank:]' ' ')
+#input=$(echo $input | tr -s '[:blank:]' ' ')
 
-# # \/ This Inserts a newline character every 64 characters
-contents=$(echo $contents | fold -w 64 -s | sed 's/^/  "/;s/$/"/')      
+
+# insert new quoted lines every 64 characters 
+#output=$(echo "$input" | fold -w 64 -s | sed 's/^/"/; s/$/"/')
+
+# write output to file
+#echo "$output" > defaultX4.txt
+
+
+# PHASE 6) add DNS header
+# # \/ This Reads the contents of defaultx.txt into a variable
+#contents=$(cat defaultX4.txt)
+    
 
 # # \/ This Adds the DNS record prefix
 #contents="default._domainkey  IN  TXT   (\n$contents\n)"
 
 # # \/ This Outputs the reformatted contents
-echo -e $contents
+#echo $contents > defaultX5.txt
 
-#--------------------
-# ./DKIM.sh
-
-
-
-# # \/ This reads input file into variable
-input=$(cat new_mod.txt)
-
-# # \/ This removes newlines and replace with space
-input=${input//$'\n'/ }
-
-# # \/ This inserts quotes at beginning and end of input
-input="\"$input\""
-
-# # \/ This inserts newlines every 255 characters
-output=$(echo "$input" | fold -w 64 -s | sed 's/^/"/; s/$/"/')
-
-# # \/ This writes output to file
-echo "$output" > new_mod2.txt
-
-# # \/ This prints the contents of the new_mod2.txt file
-cat new_mod2.txt
-
-#___________________________________________________
-
-# # # This command 
-sudo sed -i 's/\(\w\{9\}\)\s\{2\}/\1/' mod.txt
-sudo sed -i 's/\(\w\{9\}\)\s\{2\}/\1/' new_mod.txt
-
-# \/\/ This command takes the contents of the file mod.txt, 
-#      removes all newline characters from it, and then writes 
-#      the resulting output to a new file named new_mod.txt.
-cat mod.txt | tr -d '\n' > new_mod.txt
-
-# # # This command 
-# gold
-sed 's/\s//g' mod.txt > new_mod.txt
-
-# # # This command 
-
-
-# # # This command 
-
-
-# # # This command 
-
-
-# # # This command 
-
-
-# # # This command 
-
-
-# # # This command 
-
-<<comment
-#----------------------------------------------------------
-
-
-# # \/ This Reads the contents of the file into a variable
-# dkim=$(cat defaultx.txt)
-
-# # \/ This Removes any newlines from the variable
-# dkim=${dkim//$'\n'/}
-
-# # \/ This Inserts a newline character after every 255 characters
-# dkim=$(echo $dkim | sed 's/.\{255\}/&\n/g')
-
-# # \/ This Adds double quotes around the DKIM record
-# dkim='"'$dkim'"'
-
-# # \/ This Prints the reformatted DKIM record
-# echo "default._domainkey  IN  TXT   ($dkim)"
-
-#----------------------------------------------------------
-comment
-###
-
-#ADD A VAR
-# echo "#variable" >> /etc/springboard/vArs/variable.txt
-#
-#CALL A VAR
-# #variable=$(cat /etc/springboard/vArs/variable.txt)
-
+#cat defaultX5.txt
 ###
 sleep 1
 echo "the script has concluded."
