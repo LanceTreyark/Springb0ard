e2bacf850f1ba00adcf8c251970f5280a5690a7d
LanceTreyark <github@treyark.com>
2023-03-26 18:16:41 -0700

    03.26.23 06:16PM

--- Springb0ard_v2_BetaMax/programFiles/v2.0a4a_addScp3r.sh
+++ Springb0ard_v2_BetaMax/programFiles/v2.0a4a_addScp3r.sh
@@ -6,3 +6,17 @@
 echo  "Creating server side ssh keys to add to your authorized users file:"
 ssh-keygen
 
+# add the default vAr file so we dont get an error notification the first time the script is run.
+touch /etc/springb0ard/vArs/defaultScpAddr.txt
+
+
+
+## Add program file:
+curl -o /tmp/v2.0a1a_springb0ardManager2.sh "url"
+#make exe
+sudo chmod +x /tmp/v2.0a1a_springb0ardManager2.sh
+#mv to dir
+sudo mv /tmp/v2.0a1a_springb0ardManager2.sh /etc/springb0ard/programFiles/
+
+#add alias command 'scp3r'
+echo 'alias newAlias="sh /etc/springb0ard/programFiles/v2.0a1a_springb0ardManager2.sh"' >> /home/$sudoUser/.bash_aliases
