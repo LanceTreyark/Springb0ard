ea4b73c9841f047f71ec89b4f2f91848b0929819
LanceTreyark <github@treyark.com>
2023-03-30 12:02:28 -0700

    03.30.23 12:02PM

--- Springb0ard_v2_BetaMax/programFiles/v2.0a1a_springb0ardManager.sh
+++ Springb0ard_v2_BetaMax/programFiles/v2.0a1a_springb0ardManager.sh
@@ -25,32 +25,44 @@ crontab will run git pull, then this script, as this changes all new aliases and
 comment
 # programFiles
 # sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/
-sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a1a_springb0ardManager.sh 
+sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a1a_springb0ardManager.sh # CORE FUNCTION
 sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a2a_installPostfix.sh # Depreciated
 sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a2b_Postfix.DKIM.sh # Depreciated
 sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a3a_basicWebServer.sh # Depreciated
 sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a3b_configBasicWebServer.sh # Depreciated
 sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a3c_deploySimpleLandingPage.sh # Depreciated
-sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a4a_addScp3r.sh # Depreciated? if not # alias= install-scp3r
-sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a4b_Scp3r.sh # alias= scp3r
-sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a4c_qscp.sh # alias= qscp
-sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a4d_cronTabAutomation.sh # alias= addcron
+sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a4a_addScp3r.sh #ADDED# Depreciated? if not # alias= install-scp3r
+sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a4b_Scp3r.sh #ADDED# alias= scp3r
+sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a4c_qscp.sh #ADDED# alias= qscp
+sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a4d_cronTabAutomation.sh #ADDED# alias= add-cron
 sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a5a_yatSpringboardIni.sh # CORE FUNCTION
 sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a5b_yatGitIni.sh # CORE FUNCTION
-sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a5c_yatGitAddAcct.sh # alias= add-git-origin
-sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a7a_gitInstallPostfix.sh # alias= install-email-server
-sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a7b_gitPostfix.DKIM.sh
-sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a7c_gitBasicWebServer.sh # alias= install-webserver
-sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a7d_gitConfigBscWebServer.sh #alias= config-webserver
-sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a7e_gitDeployLandingPage.sh # alias= deploy-landing-page
-sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.1b0a_emailRootIni.sh
-sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.1c0a_gitEmailRoot.sh
-
+sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a5c_yatGitAddAcct.sh #ADDED# alias= add-origin
+sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a7a_gitInstallPostfix.sh #ADDED# alias= install-email-server
+sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a7b_gitPostfix.DKIM.sh #ADDED# alias= install-email-dkim
+sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a7c_gitBasicWebServer.sh #ADDED# alias= install-webserver
+sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a7d_gitConfigBscWebServer.sh #ADDED# alias= config-webserver
+sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a7e_gitDeployLandingPage.sh #ADDED# alias= deploy-landing-page
+sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.1b0a_emailRootIni.sh # Depreciated
+sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.1c0a_gitEmailRoot.sh # CORE FUNCTION
 # dTools
-sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/dTools/microInit_i1.sh  
-sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/dTools/microInit_v0.0.sh
-sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/dTools/Q_Com.sh
-sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/dTools/SQ_Com_v0.0.sh  
-
-echo 'alias newAlias="sh /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/x"' >> /home/$sudoUser/.bash_aliases
-
+sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/dTools/microInit_i1.sh # alias= dude
+sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/dTools/microInit_v0.0.sh # alias= sudo-dude
+sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/dTools/Q_Com.sh # alias= commit
+sudo chmod +x cd /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/dTools/SQ_Com_v0.0.sh # alias= sudo-commit
+# Aliases
+echo 'alias install-scp3r="sh /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a4a_addScp3r.sh"' >> /home/$sudoUser/.bash_aliases
+echo 'alias scp3r="sh /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a4b_Scp3r.sh"' >> /home/$sudoUser/.bash_aliases
+echo 'alias qscp="sh /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a4c_qscp.sh"' >> /home/$sudoUser/.bash_aliases
+echo 'alias add-cron="sh /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a4d_cronTabAutomation.sh"' >> /home/$sudoUser/.bash_aliases
+echo 'alias add-origin="sh /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a5c_yatGitAddAcct.sh"' >> /home/$sudoUser/.bash_aliases
+echo 'alias install-email-server="sh /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a7a_gitInstallPostfix.sh"' >> /home/$sudoUser/.bash_aliases
+echo 'alias install-email-dkim="sh /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a7b_gitPostfix.DKIM.sh"' >> /home/$sudoUser/.bash_aliases
+echo 'alias install-webserver="sh /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a7c_gitBasicWebServer.sh"' >> /home/$sudoUser/.bash_aliases
+echo 'alias config-webserver="sh /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a7d_gitConfigBscWebServer.sh"' >> /home/$sudoUser/.bash_aliases
+echo 'alias deploy-landing-page="sh /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/v2.0a7e_gitDeployLandingPage.sh"' >> /home/$sudoUser/.bash_aliases
+echo 'alias dude="sh /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/dTools/microInit_i1.sh"' >> /home/$sudoUser/.bash_aliases
+echo 'alias sudo-dude="sh /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/dTools/microInit_v0.0.sh"' >> /home/$sudoUser/.bash_aliases
+echo 'alias commit="sh /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/dTools/Q_Com.sh"' >> /home/$sudoUser/.bash_aliases
+echo 'alias sudo-commit="sh /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/dTools/SQ_Com_v0.0.sh"' >> /home/$sudoUser/.bash_aliases
+#echo 'alias newAlias="sh /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/programFiles/x"' >> /home/$sudoUser/.bash_aliases
