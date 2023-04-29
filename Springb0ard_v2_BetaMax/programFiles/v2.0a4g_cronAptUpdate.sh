0ced5f667d2db8414a2f840fbd17c33237f60296
LanceTreyark <github@treyark.com>
2023-04-29 05:22:11 -0700

    04.29.23 05:21AM cleaned up

--- Springb0ard_v2_BetaMax/programFiles/v2.0a4g_cronAptUpdate.sh
+++ Springb0ard_v2_BetaMax/programFiles/v2.0a4g_cronAptUpdate.sh
@@ -17,37 +17,14 @@ fi
 touch $LOCKFILE
 
 # Run system update Script
-
-#Call your vArs!
-yourDomain=$(cat /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/vArs/mailDomain.txt)
-mailDomain=$(cat /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/vArs/mailDomain.txt)
-regMailUser=$(cat /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/vArs/regMailUser.txt)
-sudoUser=$(cat /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/vArs/sudoUser.txt)
-sudoUserID=$(cat /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/vArs/sudoUserID.txt)
-myIP=$(cat /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/vArs/myIP.txt)
-webAdminEmail=$(cat /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/vArs/webAdminEmail.txt)
-webDomainName=$(cat /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/vArs/mailDomain.txt)
-defaultScpAddr=$(cat /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/vArs/defaultScpAddr.txt)
-scpExportPath=$(cat /etc/springb0ard/Springb0ard/Springb0ard_v2_BetaMax/vArs/scpExportPath.txt)
-echo "           vArs Test:"
-echo "-----------------------------"
-echo "yourDomain=$yourDomain"
-echo "mailDomain=$mailDomain"
-echo "regMailUser=$regMailUser"
-echo "sudoUser=$sudoUser"
-echo "sudoUserID=$sudoUserID"
-echo "myIP=$myIP"
-echo "webAdminEmail=$webAdminEmail"
-echo "webDomainName=$webDomainName"
-echo "defaultScpAddr=$defaultScpAddr"
-echo "scpExportPath=$scpExportPath"
-echo "-----------------------------"
+  
 todaysDate=$(date +%m%d%y)
 timeNow=$(date +%I%M%p)
+echo "Today is $todaysDate, the time is $timeNow"
+echo "Running apt-update & upgrade..."
 sudo apt update
 sudo apt upgrade -y
+echo "$(date) system update ran successfully"
 echo "$(date) system update ran successfully" >> /etc/springb0ard/springb0ardUpdate.log
 # Remove lock file
 rm $LOCKFILE
-
-echo "$(date) System update ran successfully"
