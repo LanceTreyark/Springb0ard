f3c321799a37f94b3d173450208ca6bec1d4d778
LanceTreyark <github@treyark.com>
2023-05-03 06:48:59 -0700

    05.03.23 06:48AM

--- Springb0ard_v2_BetaMax/programFiles/v2.0a3a_testArea.sh
+++ Springb0ard_v2_BetaMax/programFiles/v2.0a3a_testArea.sh
@@ -4,6 +4,7 @@
 # sb-test         --Run test script
 # sb-etest        --Edit test script
 
+# IPV6 -In
 myIPv6=$(ip addr show | awk '/inet6 .* scope global/ {split($2, arr, "/"); print arr[1]}')
 cat >/tmp/ipSorter.txt <<EOF
 $myIPv6
@@ -11,15 +12,4 @@ EOF
 myIP6=$(awk '{print $1}' /tmp/ipSorter.txt)
 echo "The IPv6 address for this server is: $myIP6"
 rm -r /tmp/ipSorter.txt
-
-<<comment
-# IPV6 -In
-myIPv6=$(ip addr show | awk '{if ($2 ~ /^fe80:/) {gsub(/\/[0-9]+/,"",$2); print $2}}' | head -1)
-cat >/tmp/ipSort3r.txt <<EOF
-$myIPv6
-EOF
-myIP6=$(awk '{print $1}' /tmp/ipSort3r.txt)
-echo "The IPv6 address for this server is: $myIP6"
-sudo rm -r /tmp/ipSort3r.txt
 # IPV6 -Out
-comment
