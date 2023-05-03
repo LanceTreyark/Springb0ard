1113d62a330c813d4b1be5cdde77bb85eb3aa91b
LanceTreyark <github@treyark.com>
2023-05-03 06:30:18 -0700

    05.03.23 06:29AM ipv6 vArs Test

--- Springb0ard_v2_BetaMax/programFiles/v2.0a3a_testArea.sh
+++ Springb0ard_v2_BetaMax/programFiles/v2.0a3a_testArea.sh
@@ -3,4 +3,13 @@
 # you can execute or edit this script with these alias commands:
 # sb-test         --Run test script
 # sb-etest        --Edit test script
-#
+
+# IPV6 -In
+myIPv6=$(ip addr show | awk '{if ($2 ~ /^fe80:/) {gsub(/\/[0-9]+/,"",$2); print $2}}' | head -1)
+cat >/tmp/ipSort3r.txt <<EOF
+$myIPv6
+EOF
+myIP6=$(awk '{print $1}' /tmp/ipSort3r.txt)
+echo "The IPv6 address for this server is: $myIP6"
+sudo rm -r /tmp/ipSort3r.txt
+# IPV6 -Out
